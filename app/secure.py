518494a802c167aed3a723dddcb1c721277b28f8
isaac.pan <hellopanan@gmail.com>
2019-01-25 23:27:24 +0800

    Delete secure.py

--- app/secure.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Hello#1234@47.101.132.11:3306/casino'
-SECRET_KEY = '\xf0\xd5{\xd7\x0f\xfe\x10\x00~I \x92|\x1a\x01=\xe9x\xecQ\x85\x0f\x1a\xe4\x82\xa4\xc1\xdawfo\xe4\xff\x1dB\xd0~\x83l?\x96'
-
